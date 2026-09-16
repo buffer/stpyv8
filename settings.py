85b810c4c71075b9cc667282042fae616b4fcd61
Angelo Dell'Aera <buffer@olografix.org>
2026-09-16 18:04:07 +0200

    Minor linking fixes

--- settings.py
+++ settings.py
@@ -176,6 +176,8 @@ elif os.name in ("posix",):
         extra_compile_args.append("-std=c++20")
         # extra_compile_args.append("-m64")
         # extra_link_args.append("-m64")
+        extra_link_args.append("-L/usr/lib/x86_64-linux-gnu")
+        extra_link_args.append("-L/usr/local/lib")
         extra_link_args.append("-fuse-ld=lld")
     else:
         os.environ["MACOSX_DEPLOYMENT_TARGET"]="14.0"
